173c101e7946a01140607e348f089f4ad37edcaa
PavanPiku <mspavankumar683@gmail.com>
2026-03-17 19:17:27 +0530

    character_replacement

---
+++ L2.py
@@ -0,0 +1,17 @@
+def characterReplacement(s, k):
+    count = {}
+    maxf = 0
+    left = 0
+    res = 0
+
+    for right in range(len(s)):
+        count[s[right]] = 1 + count.get(s[right], 0)
+        maxf = max(maxf, count[s[right]])
+
+        while (right - left + 1) - maxf > k:
+            count[s[left]] -= 1
+            left += 1
+
+        res = max(res, right - left + 1)
+
+    return res
