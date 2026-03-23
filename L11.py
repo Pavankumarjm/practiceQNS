b7d33bd27875d887c59d5548acfa276ccc85a424
PavanPiku <mspavankumar683@gmail.com>
2026-03-23 21:31:56 +0530

    Perfect_Number

---
+++ L11.py
@@ -0,0 +1,16 @@
+def check_perfect_number(num):
+    if num <= 1:
+        return False
+
+    total = 1
+
+    i = 2
+    while i * i <= num:
+        if num % i == 0:
+            total += i
+
+            if i != num // i:
+                total += num // i
+        i += 1
+
+    return total == num
